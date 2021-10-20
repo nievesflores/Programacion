2c5ca834b6ea45c638e6d29412269f2aaa7933b8
nievesflores <92874715+nievesflores@users.noreply.github.com>
2021-10-20 11:22:16 -0400

    Add files via upload

---
+++ corte 1 Nieves Flores.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace programacion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //fecha de cumpleaños 25 de agosto = 25-8
+            int a = 2;
+            int b = 5;
+            int c = 8;
+
+            //aacb
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.WriteLine(b);
+
+            Console.WriteLine("----");
+            
+            //bca
+            Console.Write(b);
+            Console.Write(c);
+            Console.WriteLine(a);
+
+            Console.WriteLine("----");
+
+            //bcbc
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("----");
+            
+            
+            
+            
+        }
+    }
+}
